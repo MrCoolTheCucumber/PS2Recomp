45245552ac2530f5f704a0eed7048f0231d338d7
Ruben <me@mrcoolthecucumber.com>
2026-07-28 12:29:08 +0100

    build: complete default runner symbol stubs

--- ps2xRuntime/src/runner/register_functions.cpp
+++ ps2xRuntime/src/runner/register_functions.cpp
@@ -5,3 +5,6 @@ extern const uint32_t g_ps2RecompiledFunctionTableBase = 0x00000000u;
 extern const uint32_t g_ps2RecompiledFunctionTableEnd = 0x01000000u;
 extern const uint32_t g_ps2RecompiledFunctionTableSlotCount = (g_ps2RecompiledFunctionTableEnd - g_ps2RecompiledFunctionTableBase) >> 2;
 PS2Runtime::RecompiledFunction g_ps2RecompiledFunctionTable[g_ps2RecompiledFunctionTableSlotCount] = {};
+
+extern const uint32_t g_ps2GuestFunctionSymbolCount = 0u;
+extern const PS2GuestFunctionSymbol g_ps2GuestFunctionSymbols[1] = {};
